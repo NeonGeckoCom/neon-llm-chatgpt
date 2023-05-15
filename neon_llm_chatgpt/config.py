20f162ae8b83b0f43319e0b05a30f3bf1ce2c320
Daniel McKnight <daniel@neongecko.com>
2023-05-15 16:36:09 -0700

    Refactor code into Python package with setup.py Update config handling to use `ovos-config` and match other Diana services Update README.md Add GitHub automation

---
+++ neon_llm_chatgpt/config.py
@@ -0,0 +1,24 @@
+import json
+
+from os.path import join, dirname, isfile
+from ovos_utils.log import LOG
+from ovos_config.config import Configuration
+
+
+def load_config() -> dict:
+    """
+    Load and return a configuration object,
+    """
+    legacy_config_path = "/app/app/config.json"
+    if isfile(legacy_config_path):
+        LOG.warning(f"Deprecated configuration found at {legacy_config_path}")
+        with open(legacy_config_path) as f:
+            config = json.load(f)
+        return config
+    config = Configuration()
+    if not config:
+        LOG.warning(f"No configuration found! falling back to defaults")
+        default_config_path = join(dirname(__file__), "default_config.json")
+        with open(default_config_path) as f:
+            config = json.load(f)
+    return config
